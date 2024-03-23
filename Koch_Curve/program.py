03bf915d5314a5cd1f1807a4f2131f5740e6d257
JavaConterry <conterry05@gmail.com>
2024-03-23 17:40:26 +0100

    Lab3: main logic of L-system in building fractals, result on Dragon curve

--- Koch_Curve/program.py
+++ Koch_Curve/program.py
@@ -4,38 +4,52 @@ import math
 tt.tracer(False)
 tt.speed(0); tt.delay(0)
 
-length = 15
-level = 4
+
+class Rule:
+    def __init__(self, base, rule):
+        self.base = base
+        self.rule = rule
 
 
-def build_strategy(level):
-    axiom = 'F--F--F'
-    rule = 'F+F--F+F'
-    for i in range(1, level):
-        new_axiom = ''
-        for i in range(len(axiom)):
-            if(axiom[i] == 'F'):
-                new_axiom = new_axiom+rule
-            else:
-                # new_axiom = new_axiom.join(axiom[i])
-                new_axiom = new_axiom+axiom[i]
-        axiom = new_axiom
+class RuleHandler:
+    def get_rule_from_base(rules, base):
+        for rule in rules:
+            if(rule.base == base):
+                return rule.rule
+        return 'NF'
 
-    return axiom
-    
+axiom = 'FX'
+# rules = [Rule('F', 'F+F--F+F')]
+rules = [Rule('X', 'X+YF+'), Rule('Y', '-FX-Y')]
+angle = 90
+level = int(input('level = '))
+length = 2
 
-for i in range(1,5):
-    print(build_strategy(i))
+class Fractal:
+    def __init__(self, axiom, rules, angle):
+        self.axiom = axiom
+        self.rules = rules
+        self.angle = angle
 
-def paint_by_strategy(strategy):
-    theta = 60
-    expl = {'F': f'tt.forward({length})', '+': 'tt.left(theta)', '-': 'tt.right(theta)'}
-    for v in strategy:
-        exec(expl[v])
+    def calculate(self, level):
+        for i in range(1, level):
+            new_axiom = ''
+            for a in self.axiom:
+                a_rule = RuleHandler.get_rule_from_base(self.rules, a)
+                if(a_rule != 'NF'):
+                    new_axiom = new_axiom+RuleHandler.get_rule_from_base(self.rules, a)
+                else:
+                    new_axiom = new_axiom+a
+            self.axiom = new_axiom
 
+    def paint(self):
+        expl = {'F': f'tt.forward({length})', '+': f'tt.left({self.angle})', '-': f'tt.right({self.angle})', 'X': f'tt.forward({length})', 'Y': f'tt.forward({length})'}
+        for v in self.axiom:
+            exec(expl[v])
 
-# length = 100//(length**(level-1) * (2+math.sqrt(2))**level)
-# print(length)
-paint_by_strategy(build_strategy(level))
+fractal = Fractal(axiom, rules, angle)    
+fractal.calculate(level)
+fractal.paint()
+print('finished')
 tt.update()
 tt.done()
