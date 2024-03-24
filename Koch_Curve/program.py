f17fd0ed3c31be063d255042d97f7718d6d8e5ee
JavaConterry <conterry05@gmail.com>
2024-03-24 13:22:29 +0100

    Lab3: Simplifying and two more examples

--- Koch_Curve/program.py
+++ Koch_Curve/program.py
@@ -1,9 +1,5 @@
 import turtle as tt
-import math
-
-tt.tracer(False)
-tt.speed(0); tt.delay(0)
-
+from tkinter import *
 
 class Rule:
     def __init__(self, base, rule):
@@ -18,12 +14,7 @@ class RuleHandler:
                 return rule.rule
         return 'NF'
 
-axiom = 'FX'
-# rules = [Rule('F', 'F+F--F+F')]
-rules = [Rule('X', 'X+YF+'), Rule('Y', '-FX-Y')]
-angle = 90
-level = int(input('level = '))
-length = 2
+
 
 class Fractal:
     def __init__(self, axiom, rules, angle):
@@ -43,13 +34,73 @@ class Fractal:
             self.axiom = new_axiom
 
     def paint(self):
-        expl = {'F': f'tt.forward({length})', '+': f'tt.left({self.angle})', '-': f'tt.right({self.angle})', 'X': f'tt.forward({length})', 'Y': f'tt.forward({length})'}
+        tt.clearscreen()
+        tt.tracer(False)
+        tt.speed(0); tt.delay(0)
+        expl = {'F': f'tt.forward({length})', 
+                '+': f'tt.left({self.angle})', 
+                '-': f'tt.right({self.angle})', 
+                'X': f'tt.forward({length})', 
+                'Y': f'tt.forward({length})'}
         for v in self.axiom:
             exec(expl[v])
+
+# class Window(Tk):
+#     def __init__(self, title, geometry):
+#         super().__init__()
+#         self.running = True
+#         self.geometry(geometry)
+#         self.title(title)
+#         self.protocol("WM_DELETE_WINDOW", self.destroy_window)
+#         self.canvas = Canvas(self)
+#         self.canvas.pack(side=LEFT, expand=True, fill=BOTH)
+#         self.turtle = tt.RawTurtle(tt.TurtleScreen(self.canvas))
+
+#     def update_window(self):
+#         if self.running:
+#             self.update()
+
+#     def destroy_window(self):
+#         self.running = False
+#         self.destroy()
+
+
+axiom = 'F--F--F'
+rules = [Rule('F', 'F+F--F+F')]
+angle = 60
+level = int(input('level = ')) #suggested level: 5
+length = 4
 
 fractal = Fractal(axiom, rules, angle)    
 fractal.calculate(level)
 fractal.paint()
 print('finished')
 tt.update()
+
+axiom = 'FX'
+rules = [Rule('X', 'X+YF+'), Rule('Y', '-FX-Y')]  #suggested level: 14
+angle = 90
+level = int(input('level = '))
+length = 2
+
+fractal = Fractal(axiom, rules, angle)    
+fractal.calculate(level)
+fractal.paint()
+print('finished')
+tt.update()
+
+
+axiom = 'F+F+F+F'
+rules = [Rule('F', 'FF+F+F+F+FF')]
+angle = 90
+level = int(input('level = ')) #suggested level: 5
+length = 2
+
+fractal2 = Fractal(axiom, rules, angle)    
+fractal2.calculate(level)
+fractal2.paint()
+print('finished')
+tt.update()
+
+
 tt.done()
