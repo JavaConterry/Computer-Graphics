3984b2b0fdc3ac87ed326c53d2243e5ccabd5552
JavaConterry <conterry05@gmail.com>
2024-05-08 19:00:41 +0200

    liniar interpolation

---
+++ Lab5/liniar_interpolation.py
@@ -0,0 +1,49 @@
+import numpy as np
+from PIL import Image
+
+
+class Interpolatable_Image:
+
+    def __init__(self, src='img.png'):
+        image = Image.open(src).convert("L")
+        self.img = np.array(image)
+        self.img_y, self.img_x = self.img.shape
+        self.multiply = None
+
+
+    def __expand(self, multiply=2): #can be improved by custom size
+        if(self.multiply!=multiply):
+            self.multiply = multiply
+        new_img = np.zeros(shape=(self.img_y*self.multiply-1, self.img_x*self.multiply-1))
+        for i in range(0, self.img_y):
+            for j in range(0, self.img_x):
+                new_img[i*self.multiply][j*self.multiply] = self.img[i][j]
+        self.show(new_img)
+        return new_img
+    
+
+    def show(self, spec=[]):
+        if(spec.shape != (1,0)):
+            Image.fromarray(spec).show()    
+        else: Image.fromarray(self.img).show()
+
+    
+    def liniar_interpolation(self, scale=2):
+        new_img = self.__expand(scale)
+        for i in range(0, len(new_img)-1, self.multiply):
+            for j in range(0, len(new_img[0])-1, self.multiply):
+                X1 = j; X2 = j+self.multiply
+                Y1 = i; Y2 = i+self.multiply
+                for y in range(Y1, Y2+1):
+                    for x in range(X1, X2+1):
+                        temp = self.img[int(Y1/self.multiply)][int(X1/self.multiply)]*(X2-x)*(Y2-y)+self.img[int(Y1/self.multiply)][int(X2/self.multiply)]*(x-X1)*(Y2-y)+self.img[int(Y2/self.multiply)][int(X1/self.multiply)]*(X2-x)*(y-Y1)+self.img[int(Y2/self.multiply)][int(X2/self.multiply)]*(x-X1)*(y-Y1)
+                        res = temp/((X2-X1)*(Y2-Y1))
+                        new_img[y][x] = res
+        return new_img
+
+
+inter = Interpolatable_Image('img.png')
+
+
+lin_int = inter.liniar_interpolation(scale=2)
+Image.fromarray(lin_int).show()
